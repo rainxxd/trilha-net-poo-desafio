3b28f7c5180a5c4d20059b60f64bd7b5a3bec76b
rainxxd <104349328+rainxxd@users.noreply.github.com>
2025-09-01 23:18:45 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("99999-1111", "Nokia Tijolão", "123456789", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("98888-2222", "iPhone 13", "987654321", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
